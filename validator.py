f475913a9efd2ee83d680215b0ea308f9477622b
Oscar Derby <oscar.derby3@gmail.com>
2024-12-30 15:07:58 -0500

    Added configuration file and loading of at runtime config file

--- validator.py
+++ validator.py
@@ -1,30 +1,44 @@
 # using regex for the win it's easier
 import re
+import json
 
 
-#write validation rules
-    #validate password has at least 8 characters
-def validate_length(password, length=8):
-    if len(password) > length:
+# validate password has at least length or 8 by default characters
+def validate_length(password, max, min=8):
+    if len(password) < min:
+        print("Password is too short!")
+        if len(password) > max:
+            print("Password is too long!")
+            return False
+        return False
+    else:
+        return True
+    
+# validate password contains lower case letters
+def validate_lower(password):
+    test1 = re.findall("[a-z]", password)
+    if test1:
         return True
     else:
         return False
-    #validate password containes both uppercase and lower case letters
-def validate_case(password):
+    
+# validate password contains uppercase and lower case letters
+def validate_upper(password):
     test1 = re.findall("[A-Z]", password)
-    test2 = re.findall("[a-z]", password)
-    if test1 and test2:
+    if test1:
         return True
     else:
         return False
-    #validate password has at least 1 numeric digit
+
+# validate password has at least 1 numeric digit
 def validate_numeric(password):
     test = re.findall("[0-9]", password)
     if test:
         return True
     else:
         return False
-    #validate at least one special character
+
+# validate at least one special character
 def validate_special(password):
     test1 = re.findall("[^A-Za-z0-9]", password)
     if test1:
@@ -32,24 +46,37 @@ def validate_special(password):
     else:
         return False
 
+# read rules from JSON config file
+def read_config():
+    config_file = open("rules.json", "rt")
+    config = json.loads(config_file.read())
+    return config
 
-def full_test(password):
-    #track the validation of the passowrd
+
+
+# run all validation tests
+def full_test(password, config_rules):
+    # track the validation of the passowrd
     valid = True
-    if validate_length(password,  8) == False:
-        print("\nPassword too short")
+    if validate_length(password, config_rules["maximum_length"], config_rules["minimum_length"]) == False:
         valid = False
-    if validate_case(password) == False:
-        print("\nPassword must contain upper and lower case")
+    if validate_lower(password) == False and config_rules["lower_case"]:
+        print("\nPassword must contain lower case")
         valid = False
-    if validate_numeric(password) == False:
+    if validate_upper(password) == False and config_rules["upper_case"]:
+        print("\nPassword must contain upper case")
+        valid = False
+    if validate_numeric(password) == False and config_rules["numbers"]:
         print("\nPassword must contain a numeric digit")
         valid = False
-    if validate_special(password) == False:
+    if validate_special(password) == False and config_rules["special_char"]:
         print("Password must contain a special character")
         valid = False
     #If the password is still valid show that it was accepted
     if valid:
         print("\nPassowrd Accepted")
 
-full_test(input("Enter your Password: "))
+config_rules = read_config()
+
+full_test(input("Enter your Password: "), config_rules)
+
